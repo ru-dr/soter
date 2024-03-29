67e92e2207040a3a841108e8f42f62456be10909
Mahavir-2003 <patelmahavir2003@outlook.com>
2024-03-29 17:51:56 +0530

    Add mongoose dependency and API endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import { Elysia } from "elysia";
 const app = new Elysia();
 
 app.get("/", () => "Hello, Elysia 🦊");
+app.get("/api", (req) => {
+  const { lng, lat } = req.query;
+  return `Hello, ${lng} ${lat}!`;
+});
 
 Bun.serve({
   port: 3000,
